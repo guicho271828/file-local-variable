2bac2822a89bcc7569deb959e5a55738e5137e75
Masataro Asai <guicho2.71828@gmail.com>
2016-02-06 23:17:16 +0900

    [Success!] overwritten the readtable to ignore all forms

--- src/package.lisp
+++ src/package.lisp
@@ -13,106 +13,59 @@
   (:export
    #:file-local-bind
    #:file-local-unbind))
+
 (in-package :file-local-variable)
 
+;; sb-int:base-char-code-limit
+;; char-code-limit
+
+(defvar *dumb-readtable*
+    (iter (with srt = (copy-readtable nil))
+          (with rt = (copy-readtable nil))
+          (for code from 0 below
+               ;; #+sbcl sb-int:base-char-code-limit
+               ;; #-(or sbcl)
+               char-code-limit)
+          (set-syntax-from-char (code-char code) #\Space rt srt)
+          (finally (return rt))))
+
 ;; blah blah blah.
-
-(setf (fdefinition 'read-original) (fdefinition 'read))
-(setf (fdefinition 'read-char-original) (fdefinition 'read-char))
-
-(defun skip-and-read (n &rest args)
-  "Overriding READ does not work, since SBCL uses internal %read-preserving-whitespace."
-  (unwind-protect
-      (let ((*read-suppress* t))
-        (dotimes (i n)
-          (print (apply #'read-original args))))
-    (#+sbcl sb-ext:without-package-locks
-     #-sbcl progn
-     (setf (fdefinition 'read)
-           (fdefinition 'read-original)))))
-
-(defun skip-and-read-char (n &rest args)
-  "Overriding READ-CHAR does not work either, since it has a buffer and several characters are already read."
-  (unwind-protect
-      (progn
-        (dotimes (i n)
-          (print (apply #'read-char-original args)))
-        (print (apply #'read-char-original args)))
-    (#+sbcl sb-ext:without-package-locks
-     #-sbcl progn
-     (setf (fdefinition 'read-char)
-           (fdefinition 'read-char-original)))))
-
-(defun skip-and-read (n &rest args)
-  (unwind-protect
-      (let ((*read-suppress* t))
-        (dotimes (i n)
-          (print (apply #'read-original args))))
-    (#+sbcl sb-ext:without-package-locks
-     #-sbcl progn
-     (setf (fdefinition 'read)
-           (fdefinition 'read-original)))))
 
 (defmacro file-local-bind (variable value &rest more &environment env)
   (recompile-with-flv (list* variable value more) env))
 
 (defun recompile-with-flv (bindings env)
   (format t "~&; rereading file ~s" *compile-file-pathname*)
-  (progv (iter (for (var val) on bindings by #'cddr)
-               (collect var))
-      (iter (for (var val) on bindings by #'cddr)
-            (collect (eval var)))
-    (with-open-file (in *compile-file-pathname* :direction :input)
-      (print (file-length in))
-      (let* ((skipped 0)
-             (start 0)
-             (end 0)
-             (expansions
-              (iter (with stage = :before)
-                    (for form in-stream in)
-                    (ematch* (form stage)
-                      (((list* 'file-local-bind _) :before)
-                       (setf start (file-position in))
-                       (format t "~&; skipped ~a forms." skipped)
-                       (format t "~&; file-local-bind found, compiling the remaining forms with variables bound")
-                       (setf stage :bound))
-                      ;; (((list* 'file-local-unbind _) :bound)
-                      ;;  (format t "~&; file-local-unbind found, checking the remaining forms")
-                      ;;  (setf stage :after))
-                      ((_ :before)
-                       (let ((*print-length* 2) (*print-level* 1))
-                         (format t "~&; skipping ~a" form))
-                       (incf skipped))
-                      ((_ :bound)
-                       (let ((*print-length* 2) (*print-level* 1))
-                         (format t "~&; expanding ~a" form))
-                       (collect (macroexpand-all form env)))
-                      (((list* 'file-local-bind _) _)
-                       (error "Found a file-local-bind twice!"))
-                      ;; ((_ :after)
-                      ;;  (error "Found a form after file-local-unbind!"))
-                      ;; (((list* 'file-local-unbind _) :before)
-                      ;;  (error "Found a file-local-unbind before file-local-bind!"))
-                      )
-                    (finally
-                     (setf end (file-position in))
-                     ;; (unless (eq stage :after)
-                     ;;   (error "missing ~A!" 'file-local-unbind))
-                     ))))
-        (format t "~&; read ~a forms (char ~a - ~a, ~a characters)." (length expansions) start end (- end start))
-        (prog1
-          (print
-           `(progn
-              ,@expansions
-              (eval-when (:compile-toplevel :load-toplevel :execute)
-                (#+sbcl sb-ext:without-package-locks
-                        #-sbcl progn
-                        (setf (fdefinition 'read-char) (curry #'skip-and-read-char ,(- end start)))))))
-          (format t "~&; Resuming compilation while skipping them"))))))
-
-#+nil
-(defmacro file-local-unbind ()
-  (#+sbcl sb-ext:without-package-locks
-   #-sbcl progn
-   (setf (fdefinition 'read)
-         (fdefinition 'read-original))))
+  (format t "~&~<; binding: ~:; ~a to ~a ~:>" bindings)
+  (let* ((skipped 0)
+         (expansions
+          (progv (iter (for (var val) on bindings by #'cddr)
+                       (collect var))
+              (iter (for (var val) on bindings by #'cddr)
+                    (collect (eval val)))
+            (iter (with stage = :before)
+                  (for form in-file *compile-file-pathname*)
+                  (ematch* (form stage)
+                    (((list* 'file-local-bind _) :before)
+                     (format t "~&; skipped ~a forms." skipped)
+                     (format t "~&; file-local-bind found, compiling the remaining forms with variables bound")
+                     (setf stage :bound))
+                    ((_ :before)
+                     (let ((*print-length* 2) (*print-level* 1))
+                       (format t "~&; skipping ~a" form))
+                     (incf skipped))
+                    ((_ :bound)
+                     (let ((*print-length* 2) (*print-level* 1))
+                       (format t "~&; expanding ~a" form))
+                     (format t "*macroexpand-hook*: ~a" *macroexpand-hook*)
+                     (collect (macroexpand-all form env)))
+                    (((list* 'file-local-bind _) _)
+                     (error "Found a file-local-bind twice!")))))))
+    (format t "~&; read ~a forms." (length expansions))
+    (prog1
+      (print
+       `(progn
+          ,@expansions
+          (eval-when (:compile-toplevel :load-toplevel :execute)
+            (setf *readtable* *dumb-readtable*))))
+      (format t "~&; Resuming compilation while skipping them"))))
